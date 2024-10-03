63529ce8a8fbaae61195e3e175512b0a0621670a
Jigyansini Dalei <daleijigyansini@gmail.com>
2024-10-03 11:11:36 -0400

    added new module

---
+++ terraform-iam-standard/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "example_role" {
+  source = "./modules/iam-role"
+
+  role_name           = "example-role"
+  assume_role_policy  = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  ]
+
+  inline_policies = [
+    {
+      name   = "custom-policy"
+      policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "ec2:Describe*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+    }
+  ]
+
+  tags = {
+    Environment = "production"
+  }
+}
---
+++ terraform-iam-standard/variables.tf
@@ -0,0 +1,5 @@
+variable "environment" {
+  description = "Environment tag for IAM roles"
+  type        = string
+  default     = "production"
+}
